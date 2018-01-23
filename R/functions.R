4dea94891d683332dd72e9759150a9d9697e36fd
Afrochemist <afrochemist@gmail.com>
2018-01-22 23:32:47 -0600

    Will make changes later

--- R/functions.R
+++ R/functions.R
@@ -12,3 +12,22 @@ weight <-function(m){
   return(m * 9.81)
 }
 
+
+#'@title KE
+#'@param m The mass of an object
+#'@param v The velocity at which the object is moving
+#'@description
+#'@return
+KE <- function(m,v){
+  return((0.5) * m * v^2)
+}
+
+
+#'@title Momentum
+#'@param m The mass of the object
+#'@param v The velocity of the object
+#'@description
+#'@return
+p <- function(m,v) {
+  return (m * v)
+}
