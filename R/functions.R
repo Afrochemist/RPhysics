291c0804ac662794c17240870da6fff9f300d730
Afrochemist <afrochemist@gmail.com>
2018-01-22 22:56:07 -0600

    Will make changes later

---
+++ R/functions.R
@@ -0,0 +1,14 @@
+#Here is the list of functions
+
+
+
+
+
+#'@title Weight
+#'@param m The mass of the object
+#'@description This function claculates the weight of an object
+#'@return The product of mass and the gravity
+weight <-function(m){
+  return(m * 9.81)
+}
+
