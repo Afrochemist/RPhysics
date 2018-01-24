6f5c13fd6ce7161581cd66e4853e65679a0b69c6
Afrochemist <afrochemist@gmail.com>
2018-01-23 20:47:41 -0600

    A script with supporting equations

---
+++ R/Support.R
@@ -0,0 +1,15 @@
+#Support eqauations
+
+
+
+#'@title Delta
+#'@param xf The final value
+#'@param xi The initial value
+#'@description The function subtracts the 2nd value from the 1st value
+#'@return The difference of the final value and the initial value
+delta <- function(xi, xf){
+  return(xf - xi)
+}
+
+
+
