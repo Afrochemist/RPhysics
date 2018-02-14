b527fb73b8d2fb8d83c86a45f13b85cb689cc04e
Afrochemist <afrochemist@gmail.com>
2018-02-13 20:22:54 -0600

    Will add more functions later

--- R/Support.R
+++ R/Support.R
@@ -7,6 +7,7 @@
 #'@param xi The initial value
 #'@description The function subtracts the 2nd value from the 1st value
 #'@return The difference of the final value and the initial value
+#'@export
 delta <- function(xi, xf){
   return(xf - xi)
 }
--- R/functions.R
+++ R/functions.R
@@ -8,6 +8,7 @@
 #'@param m The mass of the object
 #'@description This function claculates the weight of an object
 #'@return The product of mass and the gravity
+#'@export
 weight <-function(m){
   return(m * 9.81)
 }
@@ -18,6 +19,7 @@ weight <-function(m){
 #'@param v The velocity at which the object is moving
 #'@description The function calculates the kinetic energy of an object
 #'@return The product of one half of the product of mass and velocity squared
+#'@export
 KE <- function(m,v){
   return((0.5) * m * v^2)
 }
@@ -32,6 +34,7 @@ KE <- function(m,v){
 #'@param a Acceleration
 #'@description This function calculates the force of an object
 #'@return The of the mass and acceleration
+#'@export
 force <- function(m, a){
   return( m * a)
 }
@@ -42,6 +45,7 @@ force <- function(m, a){
 #'@param v The velocity of the object
 #'@description The function calculates the momentum of an object
 #'@return The product of the mass and volume
+#'@export
 p <- function(m,v) {
   return (m * v)
 }
@@ -52,6 +56,7 @@ p <- function(m,v) {
 #'@param r The radius
 #'@description This function calculates the centripetal force in terms of velocity
 #'@return The ratio of the velocity squared over the ratio
+#'@export
 cav <-function(v, r){
   return((v^2)/r)
 }
@@ -65,6 +70,7 @@ cav <-function(v, r){
 #'@param r The radius
 #'@description This function calculates the centripetal force in terms of angular velocity
 #'@return The product of the negative angular velocity squared and the radius
+#'@export
 caw <-function(w, r){
   return(-w^2 * r)
 }
@@ -73,7 +79,8 @@ caw <-function(w, r){
 #'@title Frequency
 #'@param T The period
 #'@description This function calculates the frequency by taking the inverse of the period
-#'@return The inverse of the perios
+#'@return The inverse of the period
+#'@export
 frequency <- function(T){
   return( 1 / T)
 }
@@ -84,6 +91,7 @@ frequency <- function(T){
 #'@param f The frequency
 #'@description This function calculates the angular frequency
 #'@return The product of pi and the frequency multoplied by 2
+#'@export
 angular_frequency <- function(f){
   return( 2*3.14159*f)
 }
@@ -93,6 +101,7 @@ angular_frequency <- function(f){
 #'@param f The frequency
 #'@description This function calculates the period
 #'@return The inverse of the period
+#'@export
 period <- function(f){
   return (1 / f)
 }
@@ -105,6 +114,7 @@ period <- function(f){
 #'@param v The volume of an object
 #'@description This function calculates the density of an object
 #'@return The quotient of the mass over the volume
+#'@export
 density <- function(m,v){
   return( m / v)
 }
@@ -116,6 +126,7 @@ density <- function(m,v){
 #'@param a The acceleration
 #'@description This function calculates the pressure of an object
 #'@return The quotient of the force over the area
+#'@export
 pressure <- function(f,a){
   return ( F / A)
 }
