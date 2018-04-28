570ee651bc04f58890df0d060cd7a9a23ee37093
Afrochemist <afrochemist@gmail.com>
2018-04-27 23:05:23 -0500

    Adding the Thermal script to the package

---
+++ R/Thermal.R
@@ -0,0 +1,12 @@
+#This file contains equations regarding thermal physics
+
+#'@title Latent heat
+#'@param Q Heat (kJ)
+#'@param m mass (kg)
+#'@param L Latent Heat ()
+
+Latent_Heat <- function(m, L) {
+  return(m * L)
+}
+
+
