5b6b0ee437b5e9edee72afe0548fc863000d8894
Afrochemist <afrochemist@gmail.com>
2018-05-02 22:41:53 -0500

    Added more roxygen comments to latent heat

--- R/Thermal.R
+++ R/Thermal.R
@@ -4,7 +4,8 @@
 #'@param Q Heat (kJ)
 #'@param m mass (kg)
 #'@param L Latent Heat ()
-
+#'@description This function calculates the Lantent Heat
+#'@return The multiplication of mass and heat
 Latent_Heat <- function(m, L) {
   return(m * L)
 }
